e59174ffed5573e164e04a066033f9cafb09dae8
qq1367212627 <1367212627@qq.com>
2017-07-16 18:36:57 +0800

    完成楼盘的id查询，接口测试

--- src/zsc/gof/dao/PremisesDao.java
+++ src/zsc/gof/dao/PremisesDao.java
@@ -3,6 +3,8 @@ package zsc.gof.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Select;
+
 import zsc.gof.entity.Premises;
 
 /**
@@ -14,7 +16,14 @@ public interface PremisesDao {
 	/**
 	 * @author lewis
 	 * @param 动态多个查询参数
-	 * @return 楼盘的列表
+	 * @return 根据多个条件查询楼盘的列表
 	 */
 	public List<Premises> search(Map map);
+	
+	/**
+	 * @author lewis
+	 * @param 动态多个查询参数
+	 * @return 根据区域id条件查询楼盘的列表
+	 */
+	public Premises queryPremisesById(int id);
 }
---
+++ src/zsc/gof/dao/test/TestPremisesDao.java
@@ -0,0 +1,21 @@
+package zsc.gof.dao.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import zsc.gof.dao.PremisesDao;
+import zsc.gof.entity.Premises;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/config/application*.xml"})
+public class TestPremisesDao {
+	@Autowired PremisesDao dao;
+	
+	@Test public void testqueryPremisesById(){
+		Premises premises = dao.queryPremisesById(1);
+		System.out.println(premises.getBuildName());
+	}
+}
