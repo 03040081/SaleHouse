d3f2ac37284e90c7589c0e7d9ef0cb0148296df3
qq1367212627 <1367212627@qq.com>
2017-07-17 13:56:41 +0800

    完成详情页的查询测试

--- src/zsc/gof/dao/test/TestHouseDao.java
+++ src/zsc/gof/dao/test/TestHouseDao.java
@@ -1,5 +1,22 @@
 package zsc.gof.dao.test;
 
-public class TestHouseDao {
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import zsc.gof.dao.HouseDao;
+import zsc.gof.entity.House;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/config/application*.xml"})
+public class TestHouseDao {
+	@Autowired HouseDao dao;
+	@Test public void test(){
+		List<House> houses = dao.queryHouseByBuildId(1);
+		System.out.println(houses.get(0).getHouseimg().get(0).getHouseId());
+	}
 }
