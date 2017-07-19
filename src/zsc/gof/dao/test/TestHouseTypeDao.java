8cd3b41add1b51b438123634926b2074ae6f2a06
qq1367212627 <1367212627@qq.com>
2017-07-19 14:17:43 +0800

    完成楼盘类型列表，房子类型列表查询

--- src/zsc/gof/dao/test/TestHouseTypeDao.java
+++ src/zsc/gof/dao/test/TestHouseTypeDao.java
@@ -1,5 +1,7 @@
 package zsc.gof.dao.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import zsc.gof.dao.HouseTypeDao;
+import zsc.gof.entity.Housetype;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/config/application*.xml"})
@@ -15,5 +18,9 @@ public class TestHouseTypeDao {
 	
 	@Test public void test(){
 		System.out.println(dao.queryHouseTypeByHouseTypeId(6).getTypeName());
+	}
+	@Test public void testqueryHouseTypeList(){
+		List<Housetype> list =  dao.queryHouseTypeList();
+		System.out.println(list.size());
 	}
 }
